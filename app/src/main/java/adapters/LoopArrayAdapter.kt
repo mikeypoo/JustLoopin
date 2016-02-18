91f2022d3735f8c626acda14d26e3c8a79e3d591
Mike McGowan <mike.mcgowan@indiegogo.com>
2016-02-17 22:58:33 -0800

    uses an array adapter to populate loops

--- app/src/main/java/adapters/LoopArrayAdapter.kt
+++ app/src/main/java/adapters/LoopArrayAdapter.kt
@@ -1,9 +1,11 @@
 package adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.mikedmcgowan.justloopin.R
 import models.Loop
 import java.util.*
 
@@ -12,8 +14,11 @@ import java.util.*
  */
 
 class LoopArrayAdapter(context: Context, loops: ArrayList<Loop>) : ArrayAdapter<Loop>(context, 0, loops) {
-    override fun getView(position: Int, convertView: View, parent: ViewGroup): View {
-        val view = View(context)
+    val inflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val item = getItem(position)
+        val view = convertView ?: inflater.inflate(R.layout.partial_loop, parent, false)
         return view
     }
 }
--- app/src/main/java/com/mikedmcgowan/justloopin/LoopActivity.kt
+++ app/src/main/java/com/mikedmcgowan/justloopin/LoopActivity.kt
@@ -1,21 +1,24 @@
 package com.mikedmcgowan.justloopin
 
+import adapters.LoopArrayAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.view.View
 import android.widget.Button
+import android.widget.ListView
 
 import java.util.ArrayList
 
 import models.Loop
 
 class LoopActivity : AppCompatActivity() {
-    private var buttonMakeLoop: Button? = null
-    private val loops: ArrayList<Loop>? = ArrayList()
+    private var loops: ArrayList<Loop> = ArrayList()
+    private var loopAdapter: LoopArrayAdapter? = null
 
     private val onClickMakeLoop = View.OnClickListener {
         val loop = Loop()
-        loops!!.add(loop)
+        loopAdapter!!.add(loop)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +28,10 @@ class LoopActivity : AppCompatActivity() {
     }
 
     private fun setUpControlViews() {
-        buttonMakeLoop = findViewById(R.id.button_make_loop) as Button
-        buttonMakeLoop!!.setOnClickListener(onClickMakeLoop)
+        loopAdapter = LoopArrayAdapter(this, loops)
+        var loopListView = findViewById(R.id.listview_loops) as ListView
+        var buttonMakeLoop = findViewById(R.id.button_make_loop) as FloatingActionButton
+        buttonMakeLoop.setOnClickListener(onClickMakeLoop)
+        loopListView.setAdapter(loopAdapter)
     }
 }
